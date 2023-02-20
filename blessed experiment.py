70cbdfd0c303497c3ad02d650e12577a3d456edf
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2023-02-20 20:54:12 +0200

    Added loadLevel function
    Copied the loadLevel function into the Blessed test so that I can try out different ways of displaying the 2D array that is generated

--- blessed experiment.py
+++ blessed experiment.py
@@ -1,3 +1,27 @@
 import blessed
 term = blessed.Terminal()
-print('I am' + term.underline('bold') + '!')
+
+def loadLevel(): #reads from a template level and places it into a 2D array
+    level = []
+
+    f = open("template.txt","r")
+    template = f.readlines()
+    f.close()
+
+    cols = len(template[0].strip())
+    rows = len(template)
+    
+    for i in range(0,rows):
+        line = template[i]
+        tempRow = []
+        for j in range(0,cols):
+            tempRow.append(line[j])
+        level.append(tempRow)
+    return level
+
+def displayLevel():
+    print("Nothing")
+
+
+level = loadLevel()
+print(level)
